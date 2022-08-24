86665dccf1d725f68e184046f112749745a303d8
StevePsaltis <steve.psaltis@gmail.com>
2022-08-24 15:16:24 +1000

    Initial commit of new script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MXB328_2022
 Repo for MXB328 Sem2 2022
+Add some informative text...
+## Another subheading
+### A subheading
---
+++ hello_world.m
@@ -0,0 +1,7 @@
+%% hello_world.m
+% Author: Steven Psaltis
+% 24 August 2022
+% 
+% Script to print "Hello world!" to command window
+
+fprintf("Hello world!\n");
